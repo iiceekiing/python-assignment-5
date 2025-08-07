779873a1a7d2232d32e0af3f0e4e65f41a6b8262
iiceekiing <iiceekiingsocials@gmail.com>
2025-08-07 12:40:39 +0100

    add: create project answer file "answer_for_user_db_task.py", implement initial amout, user welcome interface, and empty dictionary for user  data base

---
+++ answer_for_user_db_task.py
@@ -0,0 +1,15 @@
+verification_amount = 1500
+
+data_base = {}
+
+
+menu = """
+   *---------------------------------------*
+   |      Login and Register Portal        |
+   |   ________________________________    |
+   |  commands:                            |
+   |   enter "login" to login              |
+   |   enter "register" to register        |
+   *---------------------------------------*
+"""
+print(menu)
